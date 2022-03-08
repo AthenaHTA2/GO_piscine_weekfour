f05b692bfb02e46f78bf84338b4710e09bea39fe
AthenaHTA2 <sedmakh2@gmail.com>
2022-03-08 17:29:36 +0000

    Quest 12 Exe 1

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -9,10 +9,18 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-	} else {
-		root.Right = BTreeInsertData(root.Right, data)
+	if root.Data > data {
+		if root.Left == nil {
+			root.Left = &TreeNode{Left: nil, Right: nil, Parent: root, Data: data}
+		} else {
+			BTreeInsertData(root.Left, data)
+		}
+	} else if root.Data < data {
+		if root.Right == nil {
+			root.Right = &TreeNode{Left: nil, Right: nil, Parent: root, Data: data}
+		} else {
+			BTreeInsertData(root.Right, data)
+		}
 	}
 	return root
 }
