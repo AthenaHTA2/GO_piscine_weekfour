8abe9f114ee0fb6dcd75b3d4981d74bc4d39c357
AthenaHTA2 <sedmakh2@gmail.com>
2022-03-08 20:29:47 +0000

    Quest 12 Exe 11

--- btreetransplant.go
+++ btreetransplant.go
@@ -7,13 +7,13 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 	if root.Data > node.Data {
 		swap := BTreeSearchItem(root.Left, node.Data)
 		swap.Data = rplc.Data
-		swap.Left = nil
-		swap.Right = nil
+		swap.Left = rplc.Left
+		swap.Right = rplc.Right
 	} else {
 		swap := BTreeSearchItem(root.Right, node.Data)
 		swap.Data = rplc.Data
-		swap.Left = nil
-		swap.Right = nil
+		swap.Left = rplc.Left
+		swap.Right = rplc.Right
 	}
 	return root
 }
